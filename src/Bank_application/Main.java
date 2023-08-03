02ab557b41ec527508fc397c952cbee7195ee5bb
Angadkumar07 <angadkumar70676@gmail.com>
2023-08-03 17:51:14 +0530

    Bank-application

---
+++ src/Bank_application/Main.java
@@ -0,0 +1,13 @@
+package Bank_application;
+
+import java.util.Scanner;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Operation operlation = new Operation();
+		operlation.bankinfo();
+	}
+
+}
